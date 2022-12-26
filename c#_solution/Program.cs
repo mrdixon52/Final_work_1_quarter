40f36b2fdff1f73a8b36503d0e08743b56559411
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-26 16:49:28 +0300

    Added functions that can print and fill array

---
+++ c#_solution/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] InputArray(string[] arr)
+{
+    Console.WriteLine("Enter elements of array via enter button");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToString(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.WriteLine();
+    Console.WriteLine("Printed array:");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} \t");
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Enter number of array elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+PrintArray(InputArray(array));
